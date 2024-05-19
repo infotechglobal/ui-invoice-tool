83c98f4d4d2e142aef2e13cd858ba0a4e161d00c
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-19 22:29:23 +0530

    chore: setup default export file to club all assets

---
+++ public/assets/assets.js
@@ -0,0 +1,4 @@
+export { default as loginImage } from './LoginPagePicure.png';
+export { default as Francepay } from './FrancePay.png';
+export { default as emailIcon } from './emailICon.png';
+export { default as pass } from './password.png';
