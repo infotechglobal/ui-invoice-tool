9de58478e101b217f80e88ce9e963bea12f0d782
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-19 22:28:50 +0530

    feat: sidebar component that will be dispayed after login

---
+++ components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='mr-10'>
+        <h1>this is sidebar</h1>
+        <button className='btn bg-orange-700'>submit</button>
+
+    </div>
+  )
+}
+
+export default Sidebar
