b4d823f392a525b8cb947d59e3eeb728825f3a37
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-19 22:28:04 +0530

    chore: setup configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,24 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+      colors:{
+        loginLeft:{
+          200:"#E5E5E5"
+        },
+        authContainer:{
+          200:"#F1F7FF"
+        }
+      }
+      
     },
   },
   plugins: [],
