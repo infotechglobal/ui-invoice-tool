15a81472a753825ab762d9e4e9ae3f02ecd38d22
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-22 13:43:27 +0530

    fix: structure of side bar is made, added francePay logo

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { BrandLogo } from '../public/assets/assets'
+import Image from 'next/image'
 
 function Sidebar() {
   return (
-    <div className='mr-10'>
-        <h1>this is sidebar</h1>
-        <button className='btn bg-orange-700'>submit</button>
+    <div className='mr-10 flex flex-col'>
+      <header>
+      <Image
+              src={BrandLogo}
+              width={132}
+              height={24}
+              alt="Picture of the Login page"
+              quality={100}
+              className="mt-8 mb-10 ml-6"
+            />
+      </header>
+      <section>
+
+      </section>
+
+      <footer>
+
+      </footer>
 
     </div>
   )
