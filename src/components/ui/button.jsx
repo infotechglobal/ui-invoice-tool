d00f708f1b6be6e7843408289a0efe2362145a68
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-22 13:46:33 +0530

    fix: overwrting the size of button

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -24,8 +24,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "w-[20px]",
-        
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
@@ -38,10 +37,10 @@ const buttonVariants = cva(
 const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
   return (
-    <Comp
+    (<Comp
       className={cn(buttonVariants({ variant, size, className }))}
       ref={ref}
-      {...props} />
+      {...props} />)
   );
 })
 Button.displayName = "Button"
