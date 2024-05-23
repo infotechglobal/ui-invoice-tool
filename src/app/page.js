b50e622da800f11e42258daad119aeaed55e47db
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-23 13:32:06 +0530

    fix: user can directly be redirected to login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
+'use client'
+
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from 'next/navigation'
+
+import { useEffect } from "react";
+
 
 export default function Home() {
+  const router=useRouter();
+  useEffect(() => {
+    router.push('/login');
+  }, []);
+
   return (
     <div>
-    <h1>Getting started</h1>
-    <button type="button" className=" bg-blue-400 rounded-lg">Go</button>
+      <h1>redirecting to Login page. click the link to proceed manually</h1>
+
+      <Link href='/login' className="underline text-blue-400 italic">click here</Link>
     </div>
   );
 }
