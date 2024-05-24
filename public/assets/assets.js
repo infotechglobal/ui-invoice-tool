b92665738a14331f703f817095964c58fca64f2e
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-24 18:37:28 +0530

    chore: sample files for sidbar invoice added

--- public/assets/assets.js
+++ public/assets/assets.js
@@ -1072,9 +1072,33 @@ const UserData = [
 
 ];
 
+const sampleFiles = [
+  { fileName: "Invoice_01.xls", uploadedAt: "Mis en ligne le 8 mai 2024" },
+  { fileName: "Invoice_02.xls", uploadedAt: "Mis en ligne le 9 mai 2024" },
+  { fileName: "Invoice_03.xls", uploadedAt: "Mis en ligne le 10 mai 2024" },
+  { fileName: "Invoice_04.xls", uploadedAt: "Mis en ligne le 11 mai 2024" },
+  { fileName: "Invoice_05.xls", uploadedAt: "Mis en ligne le 12 mai 2024" },
+  { fileName: "Invoice_06.xls", uploadedAt: "Mis en ligne le 13 mai 2024" },
+  { fileName: "Invoice_07.xls", uploadedAt: "Mis en ligne le 14 mai 2024" },
+  { fileName: "Invoice_08.xls", uploadedAt: "Mis en ligne le 15 mai 2024" },
+  { fileName: "Invoice_09.xls", uploadedAt: "Mis en ligne le 16 mai 2024" },
+  { fileName: "Invoice_10.xls", uploadedAt: "Mis en ligne le 17 mai 2024" },
+  { fileName: "Invoice_11.xls", uploadedAt: "Mis en ligne le 18 mai 2024" },
+  { fileName: "Invoice_12.xls", uploadedAt: "Mis en ligne le 19 mai 2024" },
+  { fileName: "Invoice_13.xls", uploadedAt: "Mis en ligne le 20 mai 2024" },
+  { fileName: "Invoice_14.xls", uploadedAt: "Mis en ligne le 21 mai 2024" },
+  { fileName: "Invoice_15.xls", uploadedAt: "Mis en ligne le 22 mai 2024" },
+  { fileName: "Invoice_16.xls", uploadedAt: "Mis en ligne le 23 mai 2024" },
+  { fileName: "Invoice_17.xls", uploadedAt: "Mis en ligne le 24 mai 2024" },
+  { fileName: "Invoice_18.xls", uploadedAt: "Mis en ligne le 25 mai 2024" },
+  { fileName: "Invoice_19.xls", uploadedAt: "Mis en ligne le 26 mai 2024" },
+  { fileName: "Invoice_20.xls", uploadedAt: "Mis en ligne le 27 mai 2024" }
+];
+
 
 export {UserData}
 export {transactions}
+export {sampleFiles}
 export { default as loginImage } from './LoginPagePicure.png';
 export { default as pic } from './pic.png';
 export { default as BrandLogo } from './BrandLogo.png';
