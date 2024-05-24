177dba1e62d10764ff4c22f247607ad7f5b80177
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-24 18:28:36 +0530

    chore: custom box shadow for file preview block added

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,23 +17,38 @@ module.exports = {
       },
     },
     extend: {
+      boxShadow: {
+        'custom': '7px 4px rgb(0,0,0)',
+      },
       colors: {
+
         loginLeft: {
           200: "#E5E5E5"
         },
         authContainer: {
           200: "#F1F7FF"
         },
+        'violet-gray-100': 'var(--VioletGray-100, #EAE7EC)',
+
         'violet-gray-900': 'var(--VioletGray-900, #403A44)',
+        'VioletGray-900': 'var(--VioletGray-900, #403A44)',
         'violet-gray-800': 'var(--VioletGray-800, #4E4852)',
+        'violet-gray-600': 'var(--VioletGray-600, #837E86)',
+        'VioletGray-500': 'var(--VioletGray-500, #908B93)',
         'bunker-neutrals-100': 'var(--Bunker-Neutrals-100, #DCDDDD)',
         'white-custom': 'var(--White, #FFF)',
         downloadButton:{
           200: '#4568DC'
         },
+
+        uploadContainerBg:{
+          200:"#4568DC"
+        }
       },
       fontFamily: {
         archivo: ['Archivo', 'sans-serif'],
+        syne: ['Syne', 'sans-serif'], 
+       
       },
       fontSize: {
         '14px': '14px',
