1b552027458c5e62085cf825eb9b8ce14d97e7e4
Saurav Gupta <er.sauravgpt@gmail.com>
2024-05-29 20:47:29 +0530

    feat: user can upload file. The uploaded file wil be visible on the screen

--- src/app/(screens)/admin/uploads/page.jsx
+++ src/app/(screens)/admin/uploads/page.jsx
@@ -1,39 +1,98 @@
-import React from 'react'
+"use client"
+
+import React, { useRef, useEffect } from 'react'
 import Header from '../../../../../components/Header'
-import { ArrowLeft, ArrowUp, Download, Search, Trash2 } from 'lucide-react'
+import { ArrowLeft, ArrowUp, Download, Search, Trash2, Upload } from 'lucide-react'
 import { sampleFiles } from '../../../../../src/lib/assets'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { DatePicker } from '../../../../../components/DatePicker'
+import {useFileStore, usefileAlert} from '../../../../../store'
+
+ 
 
 
 function Uploads({ isInvoice = true }) {
+  const addFile = useFileStore((state) => state.addNewFile)
+  const toggleLoad= usefileAlert((state)=>state.toggleState)
+  const uploadedFiles = useFileStore((state) => state.uploadedFiles)
+  const inputFileRef = useRef(null)
+
+  const handleUploadClick = () => {
+    if (inputFileRef.current) {
+      inputFileRef.current.click();
+    }
+  };
+
+  const processFile = async (event) => {
+    const selectedFile = event.target.files?.[0];
+    console.log("file ",selectedFile);
+    if (!selectedFile) {
+      console.log("No file selected.");
+      return;
+    }
+
+    const fileContent = await selectedFile.text();
+    const fileData = fileContent.split("\n"); // Split the file content by new lines
+  
+
+    const finalContent = fileData.map(row => row.split(",")); // Map each row to an array of values
+    console.log(finalContent);
+
+    const newFile = {
+      name: selectedFile.name,
+      data: finalContent.slice(1), // Exclude the header row
+      lastModified: selectedFile.lastModifiedDate
+    }
+
+
+    addFile(newFile);
+    toggleLoad();
+    setTimeout(() => {
+      toggleLoad()
+    }, 2000);
+
+  };
+
+  useEffect(() => {
+    console.log("Updated uploadedFiles state: ", uploadedFiles);
+  }, [uploadedFiles]);
+
   return (
     <div className='pt-2 pr-2 pl-3 flex flex-col '>
-      <div className="header flex flex-col">
+      <div className="header flex flex-col w- ">
         <div className='flex justify-between'>
           <div>
-
             <h3 className="text-violet-gray-900 font-archivo text-[28px] font-bold leading-[32px] normal-font-style">
               Nom du fichier 1
             </h3>
-
           </div>
-          {/* buttons */}
-          {/* <div className='flex items-end'>
-            <Button size="btn" className="bg-downloadButton-200 h-6 ">
-            <ArrowUp className='mr-2 mt-0' size={16} color="#f6faff" strokeWidth={3} />
-                Dernière mise à jour : 8 mai 2024 à 13h00
-            </Button>
-            <Button size="btn" className="bg-downloadButton-200 h-6 ml-3">
-            <ArrowLeft className='mr-2 mt-0' size={16} color="#f6faff" />Retourner
-            </Button>
-        </div> */}
+
         </div>
-        {/* seclect client */}
-        <div className='mt-1'>
+        {/* select client */}
+        <div className='mt-1 flex justify-between'>
+
           <h3 className='text-violet-gray-800 font-archivo text-custom-18 font-normal leading-custom-24'>Sélectionnez Client pour afficher les détails</h3>
+          <div
+          className="rounded-xl px-2 py-1 bg-uploadContainerBg-200 flex justify-center items-center text-black font-semibold mr-7 cursor-pointer"
+          onClick={handleUploadClick}
+        >
+          {/* <label>Upload</label> */}
+          Upload 
+          <Upload className="" size={16} />
+          <input
+            type="file"
+            name=""
+            id="inputFile"
+            className='hidden'
+            ref={inputFileRef}
+            onChange={processFile}
+            accept=".csv"
+          />
         </div>
+        
+        </div>
+      
         {/* search bar, date picker, download invoice */}
         <div className='mt-3 flex justify-between relative pr-7'>
           <Search className='absolute top-1 left-3' size={18} color="#403A44" strokeWidth={1.75} />
@@ -42,20 +101,19 @@ function Uploads({ isInvoice = true }) {
           <Button className="rounded-lg border-2 border-violet-gray-100 h-7 bg-white text-violet-gray-900">
             <Download className='mr-2 mt-0' size={16} color="black" />Télécharger des données
           </Button>
-
-
-
         </div>
       </div>
 
+     
       {/* files */}
       <div className='h-[550px] overflow-y-scroll no-scrollbar'>
-        {sampleFiles?.map((item, index) => (
+        {uploadedFiles?.map((item, index) => (
           <div key={index} className="flex items-center gap-7 self-stretch files mt-[20px]">
+            {console.log(item)}
             <div className='flex flex-col w-[1150px] bg-uploadContainerBg-200 rounded-lg p-2 space-y-4 border-black shadow-custom'>
               <div className='space-y-2'>
-                <h1 className='text-white font-archivo text-lg font-semibold leading-6'>{item.fileName}</h1>
-                <h2 className='text-white font-syne text-base font-normal leading-4'>{item.uploadedAt}</h2>
+                <h1 className='text-white font-archivo text-lg font-semibold leading-6'>{item.name}</h1>
+                <h2 className='text-white font-syne text-base font-normal leading-4'>last modified {new Date(item.lastModified).toLocaleString()}</h2>
               </div>
               <Link href='/admin/Invoices' className='text-white font-archivo text-sm font-normal leading-4 underline'>Aperçu</Link>
             </div>
