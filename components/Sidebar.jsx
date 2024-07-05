ada20bcf00c871082ccfdafeeeb151beb68c0e5e
Saurav Gupta <er.sauravgpt@gmail.com>
2024-07-05 19:41:55 +0530

    fix: improved alert ui, used loader state to display the loading

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,11 +2,12 @@
 import React, { useEffect } from 'react'
 import { BrandLogo, sampleFiles } from '../src/lib/assets'
 import Image from 'next/image'
-import { CircleCheck, CircleX, File } from 'lucide-react'
+import { CircleCheck, CircleX, File, TriangleAlert } from 'lucide-react'
 import Link from 'next/link'
-import {usefileAlert, useAlertMessage } from '../store/alertStore'
+import { useAlertMessage } from '../store/alertStore'
 import { useFileStore } from '../store/uploadedFilesStore'
-
+import Loader from '../src/components/ui/loader.jsx'
+import useLoaderStore from '../store/loaderStore'
 
 import {
   Alert,
@@ -14,20 +15,10 @@ import {
   AlertTitle,
 } from "@/components/ui/alert"
 
-
 function Sidebar() {
-
-
   const uploadedFiles = useFileStore((state) => state.uploadedFiles)
-  const message = useAlertMessage((state) => state.message);
-  const status = useAlertMessage((state) => state.status);
-  const isLoading = usefileAlert((state) => state.isLoading);
-  const toggleLoad = usefileAlert((state) => state.toggleState);
-
-
-
-
-
+  const { message, status, showAlert, hideAlert, isLoading } = useAlertMessage();
+  const isLoaderLoading = useLoaderStore((state) => state.isLoading);
 
   return (
     <div className='mr-10 flex flex-col w-full h-full justify-between'>
@@ -45,47 +36,53 @@ function Sidebar() {
           </Link>
         </header>
 
-        <section className='pl-2' >
-          <h2 className=' text-violet-gray-900 font-Archivo font-bold text-sm leading-4 w-max'>Tous les fichiers</h2>
+        <section className='pl-2'>
+          <h2 className='text-violet-gray-900 font-Archivo font-bold text-sm leading-4 w-max'>Tous les fichiers</h2>
           <div className="flex flex-col space-y-1 files ml-3 mt-3 w-max">
-
-            {uploadedFiles?.map((item, index) => {
-              return (
-                // contents
-                <div key={index} className="flex space-x-2">
-                  <div className="icon">
-                    <File className='mt-1' size={16} color="#6f6a73" strokeWidth={2.25} />
-                  </div>
-                  <div className="text-VioletGray-600 font-Archivo text-sm leading-6 w-max">
-                    {item.filename}
-                  </div>
+            {uploadedFiles?.map((item, index) => (
+              <div key={index} className="flex space-x-2">
+                <div className="icon">
+                  <File className='mt-1' size={16} color="#6f6a73" strokeWidth={2.25} />
                 </div>
-              );
-
-            })}
-
-
+                <div className="text-VioletGray-600 font-Archivo text-sm leading-6 w-max">
+                  {item.filename}
+                </div>
+              </div>
+            ))}
           </div>
         </section>
       </div>
 
       <footer className='mb-3 self-center'>
-        {isLoading &&
-          <Alert variant="success">
-            <CircleCheck className=""size={20} />
-            <div className='flex  justify-between'>
-              <AlertTitle className="">Success</AlertTitle>
-              <CircleX size={20} onClick={toggleLoad} />
+    { isLoaderLoading &&(
+    <div className='bg-blue-200 rounded-3xl border-2 border-gray-200 '>
+          <Loader />
+        </div>
+     )
+    }
+        
+        
 
+        {isLoading && (
+          <Alert variant={status}>
+            {status === 'Success' ? (
+              <CircleCheck size={20} />
+            ) : (
+              <TriangleAlert size={20} />
+            )}
+            <div className='flex justify-between'>
+              <AlertTitle>{status}</AlertTitle>
+              <CircleX size={20} onClick={hideAlert} />
             </div>
             <AlertDescription>
               {message}
             </AlertDescription>
-          </Alert>}
-      </footer>
+          </Alert>
+        )}
 
+      </footer>
     </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
