374e82f3dc696b7f29bac0e17739df68774be990
Saurav Gupta <er.sauravgpt@gmail.com>
2024-07-05 19:43:02 +0530

    fix: used alert and loader states for showing the relevant messages.

--- src/app/(screens)/admin/invoice/[id]/page.jsx
+++ src/app/(screens)/admin/invoice/[id]/page.jsx
@@ -1,6 +1,6 @@
 // components/Dashboard.jsx
 'use client'
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { SelectScrollable } from '../../../../../../components/Select';
 import { CustomTable } from '../../../../../../components/Table';
@@ -9,27 +9,31 @@ import { Navigation } from 'lucide-react';
 import axios from 'axios';
 import { useFileNameStore, useParentFolderIdStore, useCsvFolderIdStore, usePdfFolderIdStore } from '../../../../../../store/invoiceIdsStore';
 import { useParams } from 'next/navigation';
-import { useInvoiceData } from '../../../../../../store/invoiceDataStore'
-
+import { useInvoiceData } from '../../../../../../store/invoiceDataStore';
+import useLoaderStore from '../../../../../../store/loaderStore';
 
 function Dashboard() {
     const { invoiceData, setInvoiceData } = useInvoiceData();
-     // Default driveId
-    // const driveId = '1C7G0DzU1929PaQNqb6fTIDZwOFbAdZ1q'; // Default driveId
+    const { showLoader, hideLoader } = useLoaderStore();
 
     const params = useParams();
     const driveId = params.id;
     console.log('driveId in id page is ', driveId);
-
 
     const { fileName, setFileName } = useFileNameStore();
     const { parentFolderId, setParentFolderId } = useParentFolderIdStore();
     const { csvFolderId, setCsvFolderId } = useCsvFolderIdStore();
     const { pdfFolderId, setPdfFolderId } = usePdfFolderIdStore();
 
+    const openInDrive = () => {
+        if(parentFolderId!==null){
+            window.open(`https://drive.google.com/drive/folders/${parentFolderId}`);
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
+            showLoader('Fetching invoice info...');
             try {
                 console.log('Fetching invoice info...');
                 const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/getInvoiceInfo`, {
@@ -49,10 +53,10 @@ function Dashboard() {
 
                 console.log('Invoice info fetched successfully');
 
-
-
             } catch (error) {
                 console.error('Error fetching invoice info:', error);
+            } finally {
+                hideLoader();
             }
         };
 
@@ -62,9 +66,7 @@ function Dashboard() {
     return (
         <div className='pt-2 pr-2 pl-3 flex flex-col'>
             {/* Header */}
-            <Header
-                isInvoice={true}
-            />
+            <Header isInvoice={true} />
 
             {/* Main content */}
             <div className='main flex flex-col pt-5'>
@@ -77,10 +79,9 @@ function Dashboard() {
                     <Button variant="downloadBtn" size="icon">
                         <Navigation size={48} color="#403A44" strokeWidth={1.75} />
                     </Button>
-                    {/* Download button */}
-                    <Button variant="downloadBtn">Télécharger</Button>
+            
                     {/* Upload to drive button */}
-                    <Button variant="downloadBtn">Télécharger dans Drive</Button>
+                    <Button onClick={openInDrive} variant="downloadBtn">Ouvrir dans Drive</Button>
                 </div>
                 {/* Table */}
                 <section className='mt-6'>
