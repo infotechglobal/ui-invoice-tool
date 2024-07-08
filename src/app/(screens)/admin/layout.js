77f19c5f0a618e54712d1b6e9609450f8a9d83ad
Saurav Gupta <er.sauravgpt@gmail.com>
2024-07-08 11:30:41 +0530

    feat: fetched uploaded files data whenever this component is refreshed

--- src/app/(screens)/admin/layout.js
+++ src/app/(screens)/admin/layout.js
@@ -1,5 +1,19 @@
+'use client'
+import { useEffect } from "react";
 import Sidebar from "../../../../components/Sidebar";
+import axios from 'axios';
+import { useFileStore } from '../../../../store/uploadedFilesStore';
 export default function ScreensLayout({ children }) {
+  const addFile = useFileStore((state) => state.addNewFiles);
+  const fetchData = async () => {
+    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/getallfiles`);
+    console.log(data.allFiles);
+    addFile(data.allFiles);
+  }
+  useEffect(() => {
+    console.log("fetching data in Layout");
+    fetchData();
+  }, [])
   return (
         <div className="flex  bg-sidebarBG">
           <div className="w-[280px] h-[100vh] flex-shrink-0   ">
