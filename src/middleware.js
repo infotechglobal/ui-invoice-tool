f677fc1eeb35c0e0e94a2e95b37cda0b2c41d64b
Saurav Gupta <er.sauravgpt@gmail.com>
2024-07-25 20:38:49 +0530

    feat: created middlware for authentication

---
+++ src/middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+
+
+export function middleware(request) {
+  const path = request.nextUrl.pathname
+
+  // Get the token from the cookies
+  const token = request.cookies.get('token')?.value
+
+  if (!token) {
+    console.warn('Token not found in cookies')
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  try {
+ 
+    
+    return NextResponse.next()
+  } catch (error) {
+    // If verification fails, redirect to login
+    console.error('Token verification failed:', error)
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+}
+
+// Configure which routes use this middleware
+export const config = {
+  matcher: ['/admin/:path*'],
+}
