197274dac2279e7cc027ccd942e6c1d836bfbf64
Saurav Gupta <er.sauravgpt@gmail.com>
2024-07-29 19:38:47 +0530

    chore: directly navigating

--- src/middleware.js
+++ src/middleware.js
@@ -2,20 +2,24 @@ import { NextResponse } from 'next/server'
 
 
 export function middleware(request) {
+  console.log('Middleware:', request.url.pathname)
   const path = request.nextUrl.pathname
+  const cookies = request.cookies.getAll();
+  console.log('Cookies:', cookies); 
+  cookies.forEach(cookie => {
+    console.log(`${cookie.name}: ${cookie.value}`);
+  });
 
   // Get the token from the cookies
   const token = request.cookies.get('token')?.value
   console.log("token", token);
 
-  if (!token) {
-    console.warn('Token not found in cookies')
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+  // if (!token) {
+  //   console.warn('Token not found in cookies')
+  //   return NextResponse.redirect(new URL('/login', request.url))
+  // }
 
   try {
- 
-    
     return NextResponse.next()
   } catch (error) {
     // If verification fails, redirect to login
