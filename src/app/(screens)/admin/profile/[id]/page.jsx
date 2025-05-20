d56d86b9466f501b914579e1741f9099933ededd
Saurav Gupta <er.sauravgpt@gmail.com>
2025-05-20 22:56:28 +0530

    showing a loader whiile fetching the customer details

--- src/app/(screens)/admin/profile/[id]/page.jsx
+++ src/app/(screens)/admin/profile/[id]/page.jsx
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button';
 import { ArrowLeftRight, ArrowLeft, Search } from 'lucide-react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { HashLoader } from "react-spinners";
 
 import {
     DropdownMenu,
@@ -20,10 +21,12 @@ function AdminProfile({ params }) {
     const [userData, setUserData] = useState({});
     const [transactions, setTransactions] = useState([]);
     const [factures, setFactures] = useState([]);
+    const [loading, setLoading] = useState(true);
     const userId = params.id;
 
     useEffect(() => {
         const fetchUserData = async () => {
+            setLoading(true);
             try {
                 const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/getUserInfo/${userId}`);
                 const { message, transactions } = response.data;
@@ -39,6 +42,8 @@ function AdminProfile({ params }) {
                 }
             } catch (error) {
                 console.error('Error fetching user data:', error);
+            } finally {
+                setLoading(false);
             }
         };
 
@@ -99,82 +104,92 @@ function AdminProfile({ params }) {
                 </div>
             </div>
 
-            {/* Main section */}
-            <section className='flex mt-[14px]'>
-                <div className="userDetails w-[535px]">
-                    {/* Header with profile pic and details */}
-                    <header className='flex w-[535px] justify-between items-center'>
-                        <div className='mt-20 ml-9 h-max'>
-                            <h3 className='customerName'>
-                                {userData.customerName || 'XXXXXXX'}
-                            </h3>
-                            <h4 className='customerIBM'>
-                                {`IBN - ${userData.ibanNo}` || 'IBN - 0000000000'}
-                            </h4>
-                        </div>
-                        <div className='h-max mr-4'>
-                            <Image
-                                src={pic}
-                                width={220}
-                                height={200}
-                                alt="Profile Picture"
-                                quality={100}
-                                className="relative top-6"
-                            />
-                        </div>
-                    </header>
-
-                    {/* Invoices section */}
-                    <main className='mt-3'>
-                        <h1 className='customerName text-left'>Factures</h1>
-                        <div className='invoiceDetails h-[354px] overflow-y-scroll no-scrollbar'>
-                            {factures?.map((item, index) => (
-                                <div key={index} className="flex justify-between border-2 border-gray-200 rounded p-2">
-                                    <section className='flex flex-col gap-2'>
-                                    <h4 className='text-VioletGray-900 font-Archivo text-base font-medium'>
-                                        {item.invoiceNo}
-                                    </h4>
-                                        <p className='text-VioletGray-500 font-Archivo text-10px font-normal non-italic'>{formatDateToFrench(item.transactionDate)}</p>
-                                        </section>
-                                    <div className="text-right">
-                                        <DropdownMenu>
-                                            <DropdownMenuTrigger asChild>
-                                                <Button variant="downloadBtn">
-                                                    Ouvrir
-                                                </Button>
-                                            </DropdownMenuTrigger>
-                                            <DropdownMenuContent>
-                                                <DropdownMenuItem onSelect={openInvoice(item.csvDriveLink)}>Facture Excel</DropdownMenuItem>
-                                                <DropdownMenuItem onSelect={openInvoice(item.pdfDriveLink)}>Facture PDF</DropdownMenuItem>
-                                            </DropdownMenuContent>
-                                        </DropdownMenu>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </main>
+            {loading ? (
+                <div className="flex flex-col items-center justify-center h-[70vh]">
+                    <HashLoader color="#6366f1" size={90} />
+                    <p className="mt-8 text-lg font-medium text-gray-600">Chargement des données client en cours...</p>
+                    <p className="text-sm text-gray-500">Veuillez patienter un instant</p>
                 </div>
-
-                {/* Transactions section */}
-                <div className="transactions px-3 py-3 flex-1">
-                    <h1 className='customerName text-left mb-3'>Transactions</h1>
-                    <div className="transactionDetails h-[550px] overflow-y-scroll no-scrollbar">
-                        {transactions?.map((item, index) => (
-                            <div key={index} className="flex justify-between border-2 border-gray-200 rounded-lg gap-y-6 p-2">
-                                <h4 className='text-VioletGray-900 font-Archivo text-14px font-medium non-italic'>
-                                    {item.designation}  ({item.fileName})
-                                </h4>
-                                <div className='flex flex-col mr-7'>
-                                    <ArrowLeftRight className='w-max place-self-end' color="#908b89" strokeWidth={1.25} />
-                                    <h3 className='text-VioletGray-500 font-Archivo text-10px font-normal non-italic'>
-                                        {item.transactionDate}
+            ) : (
+                <>
+                    {/* Main section */}
+                    <section className='flex mt-[14px]'>
+                        <div className="userDetails w-[535px]">
+                            {/* Header with profile pic and details */}
+                            <header className='flex w-[535px] justify-between items-center'>
+                                <div className='mt-20 ml-9 h-max'>
+                                    <h3 className='customerName'>
+                                        {userData.customerName || 'XXXXXXX'}
                                     </h3>
+                                    <h4 className='customerIBM'>
+                                        {`IBN - ${userData.ibanNo}` || 'IBN - 0000000000'}
+                                    </h4>
                                 </div>
+                                <div className='h-max mr-4'>
+                                    <Image
+                                        src={pic}
+                                        width={220}
+                                        height={200}
+                                        alt="Profile Picture"
+                                        quality={100}
+                                        className="relative top-6"
+                                    />
+                                </div>
+                            </header>
+
+                            {/* Invoices section */}
+                            <main className='mt-3'>
+                                <h1 className='customerName text-left'>Factures</h1>
+                                <div className='invoiceDetails h-[354px] overflow-y-scroll no-scrollbar'>
+                                    {factures?.map((item, index) => (
+                                        <div key={index} className="flex justify-between border-2 border-gray-200 rounded p-2">
+                                            <section className='flex flex-col gap-2'>
+                                            <h4 className='text-VioletGray-900 font-Archivo text-base font-medium'>
+                                                {item.invoiceNo}
+                                            </h4>
+                                                <p className='text-VioletGray-500 font-Archivo text-10px font-normal non-italic'>{formatDateToFrench(item.transactionDate)}</p>
+                                                </section>
+                                            <div className="text-right">
+                                                <DropdownMenu>
+                                                    <DropdownMenuTrigger asChild>
+                                                        <Button variant="downloadBtn">
+                                                            Ouvrir
+                                                        </Button>
+                                                    </DropdownMenuTrigger>
+                                                    <DropdownMenuContent>
+                                                        <DropdownMenuItem onSelect={openInvoice(item.csvDriveLink)}>Facture Excel</DropdownMenuItem>
+                                                        <DropdownMenuItem onSelect={openInvoice(item.pdfDriveLink)}>Facture PDF</DropdownMenuItem>
+                                                    </DropdownMenuContent>
+                                                </DropdownMenu>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </main>
+                        </div>
+
+                        {/* Transactions section */}
+                        <div className="transactions px-3 py-3 flex-1">
+                            <h1 className='customerName text-left mb-3'>Transactions</h1>
+                            <div className="transactionDetails h-[550px] overflow-y-scroll no-scrollbar">
+                                {transactions?.map((item, index) => (
+                                    <div key={index} className="flex justify-between border-2 border-gray-200 rounded-lg gap-y-6 p-2">
+                                        <h4 className='text-VioletGray-900 font-Archivo text-14px font-medium non-italic'>
+                                            {item.designation}  ({item.fileName})
+                                        </h4>
+                                        <div className='flex flex-col mr-7'>
+                                            <ArrowLeftRight className='w-max place-self-end' color="#908b89" strokeWidth={1.25} />
+                                            <h3 className='text-VioletGray-500 font-Archivo text-10px font-normal non-italic'>
+                                                {item.transactionDate}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                </div>
-            </section>
+                        </div>
+                    </section>
+                </>
+            )}
         </div>
     );
 }
