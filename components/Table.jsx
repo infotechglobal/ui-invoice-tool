624881fe34452bf639683eb040032a9b4db79a86
Saurav Gupta <er.sauravgpt@gmail.com>
2025-05-20 22:55:27 +0530

    changed cursor to pointer

--- components/Table.jsx
+++ components/Table.jsx
@@ -40,7 +40,7 @@ export function CustomTable({ invoiceData }) {
 
     return (
         <>
-            <Table>
+            <Table className="cursor-pointer">
                 <TableHeader>
                     <TableRow>
                         {/* {
