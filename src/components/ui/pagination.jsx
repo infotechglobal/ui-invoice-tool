6df982e95f2991aa0958fe7e5ffaed97f1dbf042
Saurav Gupta <er.sauravgpt@gmail.com>
2025-08-31 22:44:35 +0530

    fix: text into French

--- src/components/ui/pagination.jsx
+++ src/components/ui/pagination.jsx
@@ -55,7 +55,7 @@ const PaginationPrevious = ({
     className={cn("gap-1 pl-2.5", className)}
     {...props}>
     <ChevronLeft className="h-4 w-4" />
-    <span>précédente</span>
+    <span>Précédente</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = "PaginationPrevious"
